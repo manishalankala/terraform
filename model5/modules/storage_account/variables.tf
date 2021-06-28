04fc6e558dc47baf64c2b4e028df3502f6deaa48
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-06-28 19:56:09 +0200

    Update variables.tf

--- model5/modules/storage_account/variables.tf
+++ model5/modules/storage_account/variables.tf
@@ -1,1 +1,12 @@
 ,,,
+variable location {}
+
+variable resource_group_id {
+  default = ""
+  description = "Resource group ID"
+}
+
+variable resource_group_name {
+  default = ""
+  description = "Resource group name"
+}
