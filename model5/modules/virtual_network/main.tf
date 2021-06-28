568be596e1002b520448587a751c25616c644cb2
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-06-28 20:45:05 +0200

    Update main.tf

--- model5/modules/virtual_network/main.tf
+++ model5/modules/virtual_network/main.tf
@@ -7,21 +7,21 @@ resource "azurerm_virtual_network" "terraformnetwork" {
     resource_group_name = "${var.resource_group_name}"
 
     tags {
-        environment = "Terraform Demo"
+        environment = "DEV"
     }
 }
 
 # Create subnet
 resource "azurerm_subnet" "terraformsubnet" {
-    name                 = "mySubnet"
+    name                 = "devSubnet"
     resource_group_name  = "${var.resource_group_name}"
-    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    virtual_network_name = "${azurerm_virtual_network.terraformnetwork.name}"
     address_prefix       = "10.0.1.0/24"
 }
 
 # Create public IPs
 resource "azurerm_public_ip" "terraformpublicip" {
-    name                         = "myPublicIP"
+    name                         = "devPublicIP"
     location                     = "${var.location}"
     resource_group_name          = "${var.resource_group_name}"
     public_ip_address_allocation = "dynamic"
@@ -32,8 +32,8 @@ resource "azurerm_public_ip" "terraformpublicip" {
 }
 
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "myterraformnsg" {
-    name                = "myNetworkSecurityGroup"
+resource "azurerm_network_security_group" "terraformnsg" {
+    name                = "devNetworkSecurityGroup"
     location            = "${var.location}"
     resource_group_name = "${var.resource_group_name}"
 
@@ -59,12 +59,12 @@ resource "azurerm_network_interface" "terraformnic" {
     name                              = "devNIC"
     location                          = "${var.location}"
     resource_group_name               = "${var.resource_group_name}"
-    network_security_group_id         = "${azurerm_network_security_group.myterraformnsg.id}"
+    network_security_group_id         = "${azurerm_network_security_group.terraformnsg.id}"
     ip_configuration {
         name                          = "myNicConfiguration"
-        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        subnet_id                     = "${azurerm_subnet.terraformsubnet.id}"
         private_ip_address_allocation = "dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
+        public_ip_address_id          = "${azurerm_public_ip.terraformpublicip.id}"
     }
 
     tags {
