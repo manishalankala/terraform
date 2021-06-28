1f419e2ff717fda15d9891343ced32df5b65f632
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-06-28 02:21:49 +0200

    Create variables.tf

---
+++ model3/terraformprovider_azure/modules/resource_group/variables.tf
@@ -0,0 +1,15 @@
+variable "global_settings" {
+  description = "Global settings object"
+}
+
+variable "tags" {
+  description = "(Required) Map of tags to be applied to the resource"
+  type        = map(any)
+}
+
+variable "settings" {}
+
+variable "resource_group_name" {
+  description = "(Required) The name of the resource group where to create the resource."
+  type        = string
+}
