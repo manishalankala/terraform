8f2aa1184a4155f2a4d06a7440b77e64b995f4c3
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-06-30 22:03:14 +0200

    Update variables.tf

--- model3/terraformprovider_azure/modules/variables.tf
+++ model3/terraformprovider_azure/modules/variables.tf
@@ -45,17 +45,94 @@ variable "tag_project" {
 }
 
 
+variable "prefix" {
+  description = "prefix will be included in the name of some resources."
+  default     = "Netflix"
+}
+
+
+variable "virtual_network_name" {
+  description = "The name for your virtual network."
+  default     = "vnet"
+}
+
+variable "address_space" {
+  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = "10.0.10.0/24"
+}
+
+
+variable "storage_account_tier" {
+  description = "Defines the storage tier. Valid options are Standard and Premium."
+  default     = "Standard"
+}
+
+
+variable "storage_replication_type" {
+  description = "Defines the replication type to use for this storage account. Valid options include LRS, GRS etc."
+  default     = "LRS"
+}
 
 
 
+variable "vm_size" {
+  description = "Specifies the size of the virtual machine."
+  default     = "Standard_A0"
+}
+
+
+variable "image_publisher" {
+  description = "Name of the publisher of the image (az vm image list)"
+  default     = "Canonical"
+}
+
+
+
+variable "image_offer" {
+  description = "Name of the offer (az vm image list)"
+  default     = "UbuntuServer"
+}
+
+variable "image_sku" {
+  description = "Image SKU to apply (az vm image list)"
+  default     = "16.04-LTS"
+}
+
+variable "image_version" {
+  description = "Version of the image to apply (az vm image list)"
+  default     = "latest"
+}
+
+variable "admin_username" {
+  description = "Administrator user name"
+  default     = "adminuser"
+}
+
+variable "admin_password" {
+  description = "Administrator password"
+  default     = "Adminpassword123!"
+}
+
+variable "source_network" {
+  description = "Allow access from this network prefix. Defaults to '*'."
+  default     = "*"
+}
+
+
+
+
+
+######################################################################################################
 
 
 variable "global_settings" {
   description = "Global settings object"
 }
-
-
-
 
 
 locals {
