ad47c38036c155b513e95ae8a20634afd51d65a9
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-06-30 21:23:33 +0200

    Update provider.tf

--- model3/terraformprovider_azure/provider/provider.tf
+++ model3/terraformprovider_azure/provider/provider.tf
@@ -1,6 +1,21 @@
 provider "azurerm" {
-    subscription_id = "${var.azure_subscription_id}"
-    client_id = "${var.azure_client_id}"
-    client_secret = "${var.azure_client_secret}"
-    tenant_id = "${var.azure_tenant_id}"
+    subscription_id        = "${var.azure_subscription_id}"
+    client_id              = "${var.azure_client_id}"
+    client_secret          = "${var.azure_client_secret}"
+    tenant_id              = "${var.azure_tenant_id}"
+    version                = "~> 2.15" 
+    #storage_use_azuread   = true
+    
+      features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }            
 }
+
+
+  
+  
+
+
+
