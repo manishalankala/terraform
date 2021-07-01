49202a7df2113114e153266d6336796f525c76e1
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-01 12:51:24 +0200

    Update module.tf

--- model3/terraformprovider_azure/modules/virtual_network/subnets/module.tf
+++ model3/terraformprovider_azure/modules/virtual_network/subnets/module.tf
@@ -5,35 +5,35 @@ resource "azurerm_resource_group" "add_resource_group" {
   enviroment   = var.environment
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_virtual_network" "add_virtual_network" {
+  name                      = "${var.prefix}-virtual_network"
+  address_space             = ["10.0.0.0/16"]
+  location                  = var.location
+  resource_group_name       = azurerm_resource_group.add_resource_group.name
 }
 
 
 
 resource "azurerm_subnet" "add_subnet" {
-  name                 = "appgateway-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/22"]
+  name                      = "appgateway-subnet"
+  resource_group_name       = azurerm_resource_group.add_resource_group.name
+  virtual_network_name      = azurerm_virtual_network.add_virtual_network.name
+  address_prefixes          = ["10.0.1.0/22"]
 
 
 
 resource "azurerm_subnet" "add_subnet" {
   name                 = "management-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.add_resource_group.name
+  virtual_network_name = azurerm_virtual_network.add_virtual_network.name
   address_prefixes     = ["10.0.2.0/22"]
 
      
   
 resource "azurerm_subnet" "add_subnet" {
   name                 = "resource-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.add_resource_group.name
+  virtual_network_name = azurerm_virtual_network.add_virtual_network.name
   address_prefixes     = ["10.0.3.0/22"]
   
   
