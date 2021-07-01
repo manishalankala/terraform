828dfef570fe287df0729001491e68742a97ad86
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-01 16:10:49 +0200

    Update module.tf

--- model3/terraformprovider_azure/modules/virtual_network/nsg/module.tf
+++ model3/terraformprovider_azure/modules/virtual_network/nsg/module.tf
@@ -8,17 +8,113 @@ resource "azurerm_resource_group" "add_resource_group" {
 
 
 
-resource "azurerm_virtual_network" "add_virtual_network" {
-  name                     = "${var.prefix}-virtual_network
-  vnet_address_space       = ["10.0.0.0/16"]
-  location                 = var.location
-  resource_group_name      = azurerm_resource_group.add_resource_group.name
-}
+resource "azurerm_network_security_group" "add_nsg_ssh" {
+  name                = "${var.prefix}-networksecuritygroup-ssh"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.add_resource_group.name
 
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
 
+  
+  
+resource "azurerm_network_security_group" "add_nsg_http" {
+  name                = "${var.prefix}-networksecuritygroup-http"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.add_resource_group.name
 
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+  security_rule {
+    name                           = "allow-http"
+    priority                       = 101
+    direction                      = "Inbound"
+    access                         = "Allow"
+    protocol                       = "Tcp"
+    source_port_range              = "*"
+    destination_port_range         = "80"
+    source_address_prefix          = "0.0.0.0/0"
+    destination_address_prefix     = "*"
+  }
+  
+  
+  
+resource "azurerm_network_security_group" "add_nsg_https" {
+  name                = "${var.prefix}-networksecuritygroup-https"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.add_resource_group.name
+
+  security_rule {
+    name                           = "allow-https"
+    priority                       = 102
+    direction                      = "Inbound"
+    access                         = "Allow"
+    protocol                       = "Tcp"
+    source_port_range              = "*"
+    destination_port_range         = "443"
+    source_address_prefix          = "0.0.0.0/0"
+    destination_address_prefix     = "*"
+  }
+  
+  
+ resource "azurerm_network_security_group" "add_nsg_rdp" {
+  name                = "${var.prefix}-networksecuritygroup-rdp"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.add_resource_group.name
+
+  security_rule {
+    name                           = "allow-rdp"
+    priority                       = 103
+    direction                      = "Inbound"
+    access                         = "Allow"
+    protocol                       = "Tcp"
+    source_port_range              = "*"
+    destination_port_range         = "3389"
+    source_address_prefix          = "0.0.0.0/0"
+    destination_address_prefix     = "*"
+  }
+   
+   
+ 
+resource "azurerm_network_security_group" "add_nsg_ftp" {
+  name                = "${var.prefix}-networksecuritygroup-ftp"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.add_resource_group.name
+
+  security_rule {
+    name                           = "allow-ftp"
+    priority                       = 104
+    direction                      = "Inbound"
+    access                         = "Allow"
+    protocol                       = "Tcp"
+    source_port_range              = "*"
+    destination_port_range         = "21"
+    source_address_prefix          = "0.0.0.0/0"
+    destination_address_prefix     = "*"
+  }   
+
+  
+resource "azurerm_network_security_group" "add_nsg_winrm" {
+  name                             = "${var.prefix}-networksecuritygroup-winrm"
+  location                         = var.location
+  resource_group_name              = azurerm_resource_group.add_resource_group.name
+
+  security_rule {
+    name                           = "allow-ftp"
+    priority                       = 104
+    direction                      = "Inbound"
+    access                         = "Allow"
+    protocol                       = "Tcp"
+    source_port_range              = "*"
+    destination_port_range         = "5986"
+    source_address_prefix          = "0.0.0.0/0"
+    destination_address_prefix     = "*"
+  } 
+  
