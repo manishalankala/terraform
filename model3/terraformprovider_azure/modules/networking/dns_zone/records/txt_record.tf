5c3a1225152934288a274cbbdccf428de18072ba
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-01 17:58:30 +0200

    Create txt_record.tf

---
+++ model3/terraformprovider_azure/modules/networking/dns_zone/records/txt_record.tf
@@ -0,0 +1,38 @@
+# Create resource group
+
+resource "azurerm_resource_group" "add_resource_group" {
+  name         = "${var.prefix}-resource_group"
+  location     = var.location
+  tags         = var.tags
+  enviroment   = var.environment
+}
+
+# Create zone
+
+resource "azurerm_dns_zone" "add_zone_public" {
+  name                    = "netflix.com"
+  resource_group_name     = azurerm_resource_group.add_resource_group.name
+}
+
+
+# Create record 
+
+resource "azurerm_dns_txt_record" "add_dns_txtrecord" {
+  name                = "${var.prefix}-txt-record"
+  zone_name           = azurerm_dns_zone.add_zone_public.name
+  resource_group_name = azurerm_resource_group.add_resource_group.name
+  ttl                 = 300
+
+  
+  record {
+    value = "google-site-authenticator"
+  }
+
+  record {
+    value = "more site information here"
+  }
+
+  #tags = {
+  #  Environment = "Production"
+  #}
+}
