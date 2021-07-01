5c20278d75fe70fd2572d8613d2dfbc442e2bf00
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-01 13:35:10 +0200

    Update a_record.tf

--- model3/terraformprovider_azure/modules/dns_zone/records/a_record.tf
+++ model3/terraformprovider_azure/modules/dns_zone/records/a_record.tf
@@ -19,7 +19,7 @@ resource "azurerm_dns_zone" "add_zone_public" {
 
 resource "azurerm_dns_a_record" "add_dns_arecord" {
   name                    = "${var.prefix}-a-record"
-  zone_name               = azurerm_dns_zone.add_zone.name
+  zone_name               = azurerm_dns_zone.add_zone_public.name
   resource_group_name     = azurerm_resource_group.add_resource_group.name
   ttl                     = 300
   records                 = ["10.0.180.17"]
