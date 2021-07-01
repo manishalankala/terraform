f8e839ed9018e2c9e110671b2c0af4c467446a7e
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-01 19:52:26 +0200

    Create module.tf

---
+++ model3/terraformprovider_azure/modules/networking/firewall/firewall_nat_rule/module.tf
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+resource "azurerm_firewall_nat_rule_collection" "example" {
+  name                = "testcollection"
+  azure_firewall_name = azurerm_firewall.example.name
+  resource_group_name = azurerm_resource_group.example.name
+  priority            = 100
+  action              = "Dnat"
+
+  rule {
+    name = "testrule"
+
+    source_addresses = [
+      "10.0.0.0/16",
+    ]
+
+    destination_ports = [
+      "53",
+    ]
+
+    destination_addresses = [
+      azurerm_public_ip.example.ip_address
+    ]
+
+    translated_port = 53
+
+    translated_address = "8.8.8.8"
+
+    protocols = [
+      "TCP",
+      "UDP",
+    ]
+  }
+}
