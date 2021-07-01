36aa724cfaec9c5436429cf989c3a981b85a376b
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-01 15:14:23 +0200

    Update ptr_record.tf

--- model3/terraformprovider_azure/modules/dns_zone/records/ptr_record.tf
+++ model3/terraformprovider_azure/modules/dns_zone/records/ptr_record.tf
@@ -24,3 +24,4 @@ resource "azurerm_dns_a_record" "add_dns_arecord" {
   resource_group_name     = azurerm_resource_group.add_resource_group.name
   ttl                     = 300
   records                 = ["netflix.com"]
+}
