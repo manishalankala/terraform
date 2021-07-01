6c7b121181c9dd2550fdcbbcf9aa61c33bd02044
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-01 17:57:08 +0200

    Create ns_record.tf

---
+++ model3/terraformprovider_azure/modules/networking/dns_zone/records/ns_record.tf
@@ -0,0 +1,32 @@
+# Create resource group
+
+resource "azurerm_resource_group" "add_resource_group" {
+  name         = "${var.prefix}-resource_group"
+  location     = var.location
+  tags         = var.tags
+  enviroment   = var.environment
+}
+
+# Create zone
+
+resource "azurerm_dns_zone" "add_zone_public" {
+  name                    = "netflix.com"
+  resource_group_name     = azurerm_resource_group.add_resource_group.name
+}
+
+
+# Create record 
+
+resource "azurerm_dns_a_record" "add_dns_nsrecord" {
+  name                    = "${var.prefix}-ns-record"
+  zone_name               = azurerm_dns_zone.add_zone_public.name
+  resource_group_name     = azurerm_resource_group.add_resource_group.name
+  ttl                     = 300
+  records                 = ["ns1.netflix.com", "ns2.netflix.com"]
+  
+  
+  
+  #tags = {
+  #  Environment = "Production"
+  #}
+}
