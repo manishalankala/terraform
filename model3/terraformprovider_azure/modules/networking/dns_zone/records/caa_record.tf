22a35f7be7e3fb79808f94200f0e132822839cbc
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-01 17:55:43 +0200

    Create caa_record.tf

---
+++ model3/terraformprovider_azure/modules/networking/dns_zone/records/caa_record.tf
@@ -0,0 +1,53 @@
+# Create resource group
+
+resource "azurerm_resource_group" "add_resource_group" {
+  name         = "${var.prefix}-resource_group"
+  location     = var.location
+  tags         = var.tags
+  enviroment   = var.environment
+}
+
+# Create zone
+
+resource "azurerm_dns_zone" "add_zone_public" {
+  name                    = "netflix.com"
+  resource_group_name     = azurerm_resource_group.add_resource_group.name
+
+  
+# Create record 
+  
+resource "azurerm_dns_caa_record" "add_dns_caarecord" {
+  name                = "test"
+  zone_name           = azurerm_dns_zone.add_zone_public.name
+  resource_group_name = azurerm_resource_group.add_resource_group.name
+  ttl                 = 300
+
+  record {
+    flags = 0
+    tag   = "issue"
+    value = "netflix.com"
+  }
+
+  record {
+    flags = 0
+    tag   = "issue"
+    value = "netflix.net"
+  }
+  
+
+  record {
+    flags = 0
+    tag   = "issuewild"
+    value = ";"
+  }
+
+  record {
+    flags = 0
+    tag   = "iodef"
+    value = "mailto:terraform@nonexisting.tld"
+  }
+
+  #tags = {
+  #  Environment = "Production"
+  #}
+}
