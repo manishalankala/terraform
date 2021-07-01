470e7f0d9fabcd1e9e2a9707353ee5d8c5cb7d86
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-01 12:52:52 +0200

    Update module.tf

--- model3/terraformprovider_azure/modules/virtual_network/subnets/module.tf
+++ model3/terraformprovider_azure/modules/virtual_network/subnets/module.tf
@@ -23,18 +23,18 @@ resource "azurerm_subnet" "add_subnet" {
 
 
 resource "azurerm_subnet" "add_subnet" {
-  name                 = "management-subnet"
-  resource_group_name  = azurerm_resource_group.add_resource_group.name
-  virtual_network_name = azurerm_virtual_network.add_virtual_network.name
-  address_prefixes     = ["10.0.2.0/22"]
+  name                      = "management-subnet"
+  resource_group_name       = azurerm_resource_group.add_resource_group.name
+  virtual_network_name      = azurerm_virtual_network.add_virtual_network.name
+  address_prefixes          = ["10.0.2.0/22"]
 
      
   
 resource "azurerm_subnet" "add_subnet" {
-  name                 = "resource-subnet"
-  resource_group_name  = azurerm_resource_group.add_resource_group.name
-  virtual_network_name = azurerm_virtual_network.add_virtual_network.name
-  address_prefixes     = ["10.0.3.0/22"]
+  name                      = "resource-subnet"
+  resource_group_name       = azurerm_resource_group.add_resource_group.name
+  virtual_network_name      = azurerm_virtual_network.add_virtual_network.name
+  address_prefixes          = ["10.0.3.0/22"]
   
   
   delegation {
