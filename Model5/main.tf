232a6d9f9da23518399fd5d3db9adf6c93edc0bd
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-03 20:26:51 +0200

    Update main.tf

--- Model5/main.tf
+++ Model5/main.tf
@@ -1,6 +1,19 @@
 
 
 
+
+############################################ Managed Service Identity ############################################
+
+resource "azurerm_user_assigned_identity" "agw" {
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  name                = "${local.prefix}-hub-agw1-msi"
+  tags                = data.azurerm_resource_group.rg.tags
+}
+
+
+
+
 ############################################  Key Vault ###########################################
 
 data "azurerm_client_config" "current" {}
