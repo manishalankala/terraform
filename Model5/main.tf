ac623021fd7a1416e22e4e3e568b37132655790d
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-03 20:20:30 +0200

    Update main.tf

--- Model5/main.tf
+++ Model5/main.tf
@@ -126,6 +126,34 @@ resource "azurerm_application_gateway" "agw" {
 
 
 
+########################################### Diagnostic Settings ###########################################
+
+resource "azurerm_monitor_diagnostic_setting" "agw" {
+  name                       = "${local.prefix}-hub-agw1-diag"
+  target_resource_id         = azurerm_application_gateway.agw.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.add_log_workspace.id
+  dynamic "log" {
+    for_each = local.diag_appgw_logs
+    content {
+      category = log.value
+
+      retention_policy {
+        enabled = false
+      }
+    }
+  }
+
+  dynamic "metric" {
+    for_each = local.diag_appgw_metrics
+    content {
+      category = metric.value
+
+      retention_policy {
+        enabled = false
+      }
+    }
+  }
+}
 
 
 
