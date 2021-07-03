08eff970f023c5e291465d78307ddae1d462da3c
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-03 20:23:05 +0200

    Update main.tf

--- Model5/main.tf
+++ Model5/main.tf
@@ -1,6 +1,23 @@
 
 
 
+###########################################  Public IP ########################################## 
+
+
+resource "azurerm_public_ip" "agw" {
+  name                = "${local.prefix}-hub-agw1-pip1"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = data.azurerm_resource_group.rg.tags
+}
+
+
+
+
+
+
 ########################################## Application Gateway ########################################
 
 
