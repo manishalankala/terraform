ad660bec0e4e82e3e6525af5c554573f179a87f3
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-05 23:10:04 +0200

    Update main.tf

--- Model6/terraform/main.tf
+++ Model6/terraform/main.tf
@@ -1,1 +1,39 @@
 
+
+
+
+# Create a Resource Group
+resource "azurerm_resource_group" "hub-rg" {
+  name     = "${var.region}-${var.environment}-${var.app_name}-hub-rg"
+  location = var.location
+  tags = {
+    environment = var.environment
+  }
+}
+
+# Create the VNET
+resource "azurerm_virtual_network" "hub-vnet" {
+  name                = "${var.region}-${var.environment}-${var.app_name}-hub-vnet"
+  address_space       = [var.hub-vnet]
+  location              = azurerm_resource_group.hub-rg.location
+  resource_group_name   = azurerm_resource_group.hub-rg.name
+  tags = {
+    environment = var.environment
+  }
+}
+
+# Create a Gateway Subnet
+resource "azurerm_subnet" "gateway-subnet" {
+  name                 = "GatewaySubnet" # do not rename
+  address_prefixes     = [var.gateway-subnet]
+  virtual_network_name = azurerm_virtual_network.hub-vnet.name
+  resource_group_name  = azurerm_resource_group.hub-rg.name
+}
+
+# Create a Hub Subnet
+resource "azurerm_subnet" "hub-subnet" {
+  name                 = "hub-resources-subnet" # do not rename
+  address_prefixes     = [var.hub-resources-subnet]
+  virtual_network_name = azurerm_virtual_network.hub-vnet.name
+  resource_group_name  = azurerm_resource_group.hub-rg.name
+}
