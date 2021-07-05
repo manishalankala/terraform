afaf8fe91f05c157d6e45c822239ae52e03d19db
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-05 23:52:21 +0200

    Update main.tf

--- Model6/terraform/main.tf
+++ Model6/terraform/main.tf
@@ -148,13 +148,24 @@ resource "azurerm_subnet" "spoketwo-subnet" {
   resource_group_name  = azurerm_resource_group.hub-rg.name
 }
 
+resource "azurerm_virtual_network_peering" "spoke1-peering-hub" {
+  name                         = "spoke1-hub"
+  resource_group_name          = azurerm_resource_group.hub-rg.name
+  virtual_network_name         = azurerm_virtual_network.spoke1-vnet.name
+  remote_virtual_network_id    = azurerm_virtual_network.hub-vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = true
+  depends_on = [azurerm_virtual_network.spoke1-vnet, azurerm_virtual_network.hub-vnet , azurerm_virtual_network_gateway.vpn-gateway]
+}
 
 
 # Create the SPOKE2-VNET
 
 resource "azurerm_virtual_network" "spoke2-vnet" {
-  name                = "${var.region}-${var.environment}-${var.app_name}-spoke2-vnet"
-  address_space       = [var.spoke2-vnet]
+  name                  = "${var.region}-${var.environment}-${var.app_name}-spoke2-vnet"
+  address_space         = [var.spoke2-vnet]
   location              = azurerm_resource_group.hub-rg.location
   resource_group_name   = azurerm_resource_group.hub-rg.name
   tags = {
@@ -180,4 +191,15 @@ resource "azurerm_subnet" "spoketwo-subnet" {
   resource_group_name  = azurerm_resource_group.hub-rg.name
 }
 
+resource "azurerm_virtual_network_peering" "spoke2-peering-hub" {
+  name                         = "spoke2-hub"
+  resource_group_name          = azurerm_resource_group.hub-rg.name
+  virtual_network_name         = azurerm_virtual_network.spoke2-vnet.name
+  remote_virtual_network_id    = azurerm_virtual_network.hub-vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = true
+  depends_on = [azurerm_virtual_network.spoke2-vnet, azurerm_virtual_network.hub-vnet , azurerm_virtual_network_gateway.vpn-gateway]
+}
 
