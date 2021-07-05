8f0a8bcbd0e7473202caca262ad1685577679aec
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-06 01:37:04 +0200

    Update main.tf

--- Model7/terraform/main.tf
+++ Model7/terraform/main.tf
@@ -37,3 +37,59 @@ resource "azurerm_subnet" "hub-subnet" {
   virtual_network_name = azurerm_virtual_network.hub-vnet.name
   resource_group_name  = azurerm_resource_group.hub-rg.name
 }
+
+
+
+
+# Create gateway onprem to Azure 
+
+resource "azurerm_local_network_gateway" "onprem" {
+  name                = "onprem-azure"
+  resource_group_name = azurerm_resource_group.hub-rg.name
+  location            = azurerm_resource_group.hub-rg.location
+  gateway_address     = "12.13.14.15"       ## Public ip of on-prem device
+  address_space       = ["10.0.0.0/16"]     ## address range of local on-prem
+}
+
+# Create a Public IP for the Gateway
+
+resource "azurerm_public_ip" "public-gateway-ip" {
+  name                = "${var.region}-${var.environment}-${var.app_name}-gw-ip"
+  location            = azurerm_resource_group.hub-rg.location
+  resource_group_name = azurerm_resource_group.hub-rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Create VPN Gateway
+
+resource "azurerm_virtual_network_gateway" "vpn-gateway" {
+  name                = "${var.region}-${var.environment}-${var.app_name}-vpn-gw"
+  location            = azurerm_resource_group.hub-rg.location
+  resource_group_name = azurerm_resource_group.hub-rg.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp    = false
+  sku           = "Basic"
+
+  ip_configuration {
+    name                          = "${var.region}-${var.environment}-${var.app_name}-vnet"
+    public_ip_address_id          = azurerm_public_ip.public-gateway-ip.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.gateway-subnet.id
+  }
+  
+
+# Create connection between on prem
+  
+resource "azurerm_virtual_network_gateway_connection" "onpremconnection" {
+    name                       = onpremiseconnection
+    location                   = azurerm_resource_group.hub-rg.location
+    resource_group_name        = azurerm_resource_group.hub-rg.name
+    type                       = "IPsec"
+    virtual_network_gateway_id = azurerm_virtual_network_gateway.vpn-gateway.id
+    local_network_gateway_id   = azurerm_local_network_gateway.onprem.id
+    shared_key                 = var.vpn_psk #-Provided at run time or 4-v3ry-53cr37-1p53c-5h4r3d-k3y
+}
