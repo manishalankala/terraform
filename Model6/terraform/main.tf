6abd74a4d2af7e3a23c8579bcb0ca58039ef016d
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-05 23:19:45 +0200

    Update main.tf

--- Model6/terraform/main.tf
+++ Model6/terraform/main.tf
@@ -3,6 +3,7 @@
 
 
 # Create a Resource Group
+
 resource "azurerm_resource_group" "hub-rg" {
   name     = "${var.region}-${var.environment}-${var.app_name}-hub-rg"
   location = var.location
@@ -12,6 +13,7 @@ resource "azurerm_resource_group" "hub-rg" {
 }
 
 # Create the VNET
+
 resource "azurerm_virtual_network" "hub-vnet" {
   name                = "${var.region}-${var.environment}-${var.app_name}-hub-vnet"
   address_space       = [var.hub-vnet]
@@ -23,6 +25,7 @@ resource "azurerm_virtual_network" "hub-vnet" {
 }
 
 # Create a Gateway Subnet
+
 resource "azurerm_subnet" "gateway-subnet" {
   name                 = "GatewaySubnet" # do not rename
   address_prefixes     = [var.gateway-subnet]
@@ -31,9 +34,59 @@ resource "azurerm_subnet" "gateway-subnet" {
 }
 
 # Create a Hub Subnet
+
 resource "azurerm_subnet" "hub-subnet" {
   name                 = "hub-resources-subnet" # do not rename
   address_prefixes     = [var.hub-resources-subnet]
   virtual_network_name = azurerm_virtual_network.hub-vnet.name
   resource_group_name  = azurerm_resource_group.hub-rg.name
+}
+
+
+
+
+
+
+# Create a Public IP for the Gateway
+
+resource "azurerm_public_ip" "public-gateway-ip" {
+  name                = "${var.region}-${var.environment}-${var.app_name}-gw-ip"
+  location            = azurerm_resource_group.hub-rg.location
+  resource_group_name = azurerm_resource_group.hub-rg.name
+  allocation_method   = "Dynamic"
+}
+
+
+
+# Create VPN Gateway
+
+resource "azurerm_virtual_network_gateway" "vpn-gateway" {
+  name                = "${var.region}-${var.environment}-${var.app_name}-vpn-gw"
+  location            = azurerm_resource_group.hub-rg.location
+  resource_group_name = azurerm_resource_group.hub-rg.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp    = false
+  sku           = "Basic"
+
+  ip_configuration {
+    name                          = "${var.region}-${var.environment}-${var.app_name}-vnet"
+    public_ip_address_id          = azurerm_public_ip.public-gateway-ip.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.gateway-subnet.id
+  }
+
+  vpn_client_configuration {
+    address_space = ["10.3.0.0/24"]
+
+    root_certificate {
+      name = "VPNROOT"
+
+      public_cert_data = data.azurerm_key_vault_secret.vpn-root-certificate.value
+    }
+
+  }
 }
