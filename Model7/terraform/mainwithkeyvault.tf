1147578a5f50d643dd79ff3a8cde6a196ba0a248
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-06 16:19:44 +0200

    Update mainwithkeyvault.tf

--- Model7/terraform/mainwithkeyvault.tf
+++ Model7/terraform/mainwithkeyvault.tf
@@ -277,3 +277,19 @@ resource "azurerm_key_vault" "app-keyvault" {
     bypass         = "AzureServices"
   }
 }
+
+    
+    
+# Create a Secret for the VPN Root certificate
+    
+resource "azurerm_key_vault_secret" "vpn-root-certificate" {
+  depends_on=[azurerm_key_vault.app-keyvault]
+
+  name         = "vpn-root-certificate"
+  value        = filebase64(local.certificate-name)
+  key_vault_id = azurerm_key_vault.app-keyvault.id
+
+  tags = {
+    environment = var.environment
+  }
+}
