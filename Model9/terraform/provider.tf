a100d8c5317048f7d049dddec4461e2cd394584b
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-08 19:15:40 +0200

    Create provider.tf

---
+++ Model9/terraform/provider.tf
@@ -0,0 +1,13 @@
+# valid for terraform version 0.14
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.40.0"
+    }
+  }
+}
