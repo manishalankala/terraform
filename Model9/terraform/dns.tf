61bec00ff94419a1bd87ce22c518e740359d2c23
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-08 19:33:08 +0200

    Update dns.tf

--- Model9/terraform/dns.tf
+++ Model9/terraform/dns.tf
@@ -13,6 +13,9 @@ resource "azurerm_dns_zone" "mydomaincom" {
 }
 
 
+#### A – specifies IP addresses corresponding to your domain and its subdomains ###
+
+
 resource "azurerm_dns_a_record" "projectmydomain" {
   name                = "project"
   zone_name           = "${azurerm_dns_zone.mydomaincom.name}"
@@ -20,3 +23,21 @@ resource "azurerm_dns_a_record" "projectmydomain" {
   ttl                 = 300
   records             = ["127.0.0.1"]
 }
+
+
+### CNAME – specifies redirects from your domain’s subdomains to other domains/subdomains ###
+
+resource "azurerm_dns_cname_record" "awesomemydomain" {
+  name                = "awesome"
+  zone_name           = "${azurerm_dns_zone.mydomaincom.name}"
+  resource_group_name = "${azurerm_resource_group.dns_management.name}"
+  ttl                 = 300
+  record              = "dev.mydomain.com"
+}
+
+MX – specifies where the emails for your domain should be delivered.
+CNAME – specifies redirects from your domain’s subdomains to other domains/subdomains.
+TXT – used to store text-based information related to your domain. Most commonly used for storing SPF data.
+SPF – a mail validation protocol used to prevent email spoofing.
+AAAA – it maps a domain name to the IP address (IPv6) of the computer hosting the domain.
+SRV – stands for Service Record and it specifies on only an IP but also a port.
