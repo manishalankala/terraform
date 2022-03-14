f60a071f42b9c32aecae344ea860b652e8a14d32
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2022-03-14 13:31:02 +0100

    Create ns_record.tf

---
+++ Model9/terraform/modules/azure-public-dns/ns_record.tf
@@ -0,0 +1,12 @@
+resource "azurerm_dns_ns_record" "this" {
+  for_each = { for record in var.ns_recordsets :
+    record.name => record
+  }
+
+  resource_group_name = lower(var.resource_group_name)
+  zone_name           = var.zone_name
+
+  name    = lower(each.value.name)
+  ttl     = each.value.ttl
+  records = each.value.record
+}
