3e4a4b60da91e5c635666dba459e0e3b22b93f03
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2022-03-14 13:31:55 +0100

    Create txt_record.tf

---
+++ Model9/terraform/modules/azure-public-dns/txt_record.tf
@@ -0,0 +1,19 @@
+resource "azurerm_dns_txt_record" "this" {
+  for_each = { for record in var.txt_recordsets :
+    record.name => record
+  }
+
+  resource_group_name = lower(var.resource_group_name)
+  zone_name           = var.zone_name
+
+  name = lower(each.value.name)
+  ttl  = each.value.ttl
+
+  dynamic "record" {
+    for_each = each.value.record
+    content {
+      value = record.value
+    }
+  }
+
+}
