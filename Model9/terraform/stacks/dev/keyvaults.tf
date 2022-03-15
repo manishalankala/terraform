556b4fe3e6abd4a461e568dcc6b22afbea8bce08
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2022-03-16 00:59:57 +0100

    Create keyvaults.tf

---
+++ Model9/terraform/stacks/dev/keyvaults.tf
@@ -0,0 +1,6 @@
+enabled_for_disk_encryption = false
+enable_rbac_authorization   = false
+purge_protection_enabled    = true
+soft_delete_retention_days  = 7
+kv_subnet_name              = " "
+devops_subnet_id            = " "
