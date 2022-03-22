5318a97c23823132052c027494c44b67ae7a8ede
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2022-03-22 10:01:28 +0100

    Create varaibles.tf

---
+++ aws/4/varaibles.tf
@@ -0,0 +1,6 @@
+variable "region" { default = "us-east-2" }
+variable "environment" { default = "" }
+variable "project" { default = "myapp" }
+variable "platform" { default = "Devops" }
+variable "vpc_id" { default = "vpc-072ecaa60abe662d4" }
+variable "team" { default = "platform" }
