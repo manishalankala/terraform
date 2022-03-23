5001bf4d8c0001f78f8f4f2a815f6a79b2655550
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2022-03-23 12:37:13 +0100

    Update main.tf

--- gcp/1/main.tf
+++ gcp/1/main.tf
@@ -1,3 +1,5 @@
+#### Providers ####
+
 terraform {
   required_version = ">= 1.0.11"
   required_providers {
@@ -19,26 +21,8 @@ provider "google" {
   project = ""
 }
 
-variable "name" { 
-}
-variable "region" {
-  default = "us-central1"
-}
-variable "machine_type" {
-  default = "n2-standard-2"
-}
-variable "node_count" {
-  default = 1
-  type = number 
-}
-variable "cluster_name" {}
-variable "endpoint" {}
-variable "ca_cert" {}
-variable "location" {
-  default = "us-central1"
-}
-variable "pod_name" {}
-variable "image" {}
+
+
 
 
 
