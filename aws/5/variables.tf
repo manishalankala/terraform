c332b73361b813c3a921b573afe157660b3c3154
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-03-29 19:44:46 +0200

    Update variables.tf

--- aws/5/variables.tf
+++ aws/5/variables.tf
@@ -5,6 +5,15 @@ variable "AWS_REGION" {
 variable "amidev" {
   default = "ami-01d025118d8e760db"
 }
+
 variable "ami" {
   default = "ami-00399ec92321828f5"
+}
+
+variable "bucketname" {
+    type = string
+}
+
+variable "vers" {
+    type = bool
 }
